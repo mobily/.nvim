99f6b056add7581bd6bde855827fd8089597945e
Marcin Dziewulski <hello@mobily.pl>
2024-02-25 23:20:55 +0100

    feat: enable gitsigns

--- lua/lazy/plugins.lua
+++ lua/lazy/plugins.lua
@@ -1055,26 +1055,26 @@ lazy.setup {
   },
   {
     "lewis6991/gitsigns.nvim",
-    ft = "gitcommit",
-    -- init = function()
-    --   vim.api.nvim_create_autocmd(
-    --     {"BufRead"},
-    --     {
-    --       group = vim.api.nvim_create_augroup("GitSignsLazyLoad", {clear = true}),
-    --       callback = function()
-    --         vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
-    --         if vim.v.shell_error == 0 then
-    --           vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
-    --           vim.schedule(
-    --             function()
-    --               lazy.load("gitsigns.nvim")
-    --             end
-    --           )
-    --         end
-    --       end
-    --     }
-    --   )
-    -- end,
+    ft = {"gitcommit", "diff"},
+    init = function()
+      vim.api.nvim_create_autocmd(
+        {"BufRead"},
+        {
+          group = vim.api.nvim_create_augroup("GitSignsLazyLoad", {clear = true}),
+          callback = function()
+            vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
+            if vim.v.shell_error == 0 then
+              vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+              vim.schedule(
+                function()
+                  require("lazy").load({plugins = {"gitsigns.nvim"}})
+                end
+              )
+            end
+          end
+        }
+      )
+    end,
     config = function()
       require("gitsigns").setup {
         signs = {
